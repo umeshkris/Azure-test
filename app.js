54525a09f6e4f54084e9b011211303a0387bdc3c
umeshkris <umesh.kris@gmail.com>
2020-06-14 18:57:11 +0530

    corrected const word

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-cosnt port =process.env.PORT || 3000
+const  port =process.env.PORT || 3000
  const server=http.createServer((req,res)=>{
      res.write('<html><head></head><body><h1>Welcome  to nodejs !!</h1></body></html>');
  });
